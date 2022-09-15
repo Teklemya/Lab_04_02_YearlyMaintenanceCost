852963aa3746e838db2e811b18869a8164b57dca
yishak <Teklemya@mail.uc.edu>
2022-09-15 15:01:32 -0400

    inital commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args)
     {
-        double price = 321.5;
-        double tax = 0.05;
-        double salesTax = price * tax;
+        double springCost = 321.5;
+        double fallCost = 129.5;
+        double summerCost = 589.5;
+        double winterCost = 395.5;
+        double yearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Your yearly maintenance cost is " + yearlyCost );
     }
 }
